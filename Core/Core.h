a15e87be57d013f2ff9829aec1c4c8b986dbe936
Anup490 <anupbhandari490@gmail.com>
2022-02-10 11:11:54 +0530

    Added core project along with vector implementations and its test cases

---
+++ Core/Core.h
@@ -0,0 +1,23 @@
+#pragma once
+namespace Core
+{
+	struct vec3
+	{
+		double x, y, z;
+		double length;
+		vec3();
+		vec3(double f);
+		vec3(double x, double y, double z);
+		double dot(const vec3& v) const;
+		vec3 cross(const vec3& v) const;
+		void normalize();
+		vec3 operator*(const double& f) const;
+		vec3 operator+(const vec3& v) const;
+		vec3 operator*(const vec3& v) const;
+		vec3 operator-(const vec3& v) const;
+		vec3 operator-() const;
+		vec3& operator+=(const vec3& v);
+		vec3& operator*=(const vec3& v);
+		vec3& operator-=(const vec3& v);
+	};
+}
---
+++ CoreTest/CoreTest.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "Core.h"
+#include <string>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace Core;
+
+namespace CoreTest
+{
+	TEST_CLASS(CoreTest)
+	{
+	public:
+		
+		bool equal(const vec3& v1, const vec3& v2)
+		{
+			return (v1.x == v2.x) && (v1.y == v2.y) && (v1.z == v2.z);
+		}
+
+		std::string to_string(const char* chars, const vec3& v)
+		{
+			std::string s(chars);
+			s.append(std::to_string(v.x));
+			s.append(", ");
+			s.append(std::to_string(v.y));
+			s.append(", ");
+			s.append(std::to_string(v.z));
+			s.append(" || ");
+			return s;
+		}
+
+		TEST_METHOD(VectorTest)
+		{
+			vec3 a(1, 2, 3);
+			vec3 b(4, 5, 6);
+			vec3 d(0, 0, 0);
+			vec3 e(1, 1, 1);
+			vec3 f(a);
+			d += a;
+			d += b;
+			e *= a;
+			e *= b;
+			f -= b;
+			Logger::WriteMessage(to_string("d = ", d).data());
+			Assert::AreEqual(true, equal(a+b, d));
+			Logger::WriteMessage(to_string("e = ", e).data());
+			Assert::AreEqual(true, equal(a * b, e));
+			Logger::WriteMessage(to_string("f = ", f).data());
+			Assert::AreEqual(true, equal(a - b, f));
+		}
+	};
+}
